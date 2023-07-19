d9ab1322794fbc21471bb87e9c9f7657f2274723
Remya Mathews <remya.mathewskl@gmail.com>
2023-07-19 15:15:39 -0400

    Tutorial3 - Input from file

---
+++ tut3_file_input.c
@@ -0,0 +1,22 @@
+void tut3_file_input()
+{
+    TH1F *hist = new TH1F("hist", "Histogram", 20, 0, 100); //20 bins-bin interval 5 (from 0-100)
+
+    fstream file;
+    file.open("tut3_data.txt", ios::in);
+
+    double value;
+    while(1)
+    {
+        file >> value;
+        hist->Fill(value);
+        if(file.eof()) break;
+    }
+    file.close();
+
+    hist->GetXaxis()->SetTitle("Score");
+    hist->GetYaxis()->SetTitle("Frequnecy");
+
+    TCanvas *c1 = new TCanvas();
+    hist->Draw();
+}
