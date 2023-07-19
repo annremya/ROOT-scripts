2afcbcd346f16d4e1a9109086f38bfc7190a13cc
Remya Mathews <remya.mathewskl@gmail.com>
2023-07-19 11:56:35 -0400

    Tutorial1-histogram

---
+++ tut1_histogram.c
@@ -0,0 +1,18 @@
+void tut1_histogram()
+{
+    //H-histogram, 1-1 Dimension, F-float (Can use I for integer values)
+    //check ROOT reference guide-Histogram library-Histogram classes
+    //TH1F (const char *name, const char *title, Int_t nbinsx, const Double_t *xbins)
+    TH1F *hist = new TH1F("hist", "Histogram", 100, 0, 100);
+
+    hist->Fill(30);
+    hist->Fill(70);
+    hist->Fill(65);
+
+    hist->GetXaxis()->SetTitle("X Axis");
+    hist->GetYaxis()->SetTitle("Y Axis");
+
+    //Window in which histogram will be drawn
+    TCanvas *c1 = new TCanvas();
+    hist->Draw();
+}
